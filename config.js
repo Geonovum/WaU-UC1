c9a4d5813e531291e0d91bfc5180dd579aab4759
Arnoud de Boer <a.deboer@geonovum.nl>
2023-02-02 08:47:42 +0100

    Update config.js

--- config.js
+++ config.js
@@ -58,11 +58,11 @@ var respecConfig =
   //-- NB: in de werkversie uitzetten, want dan pakt Respec de pushdate ---------------
   //publishDate: "2022-07-11",  	    // Format is "YYYY-MM-DD"
   //-- Repositorynaam op GitHub -------------------------------------------------------
-  github: "https://github.com/geonovum/WaU",
+  github: "https://github.com/geonovum/WaU-UC1",
   //-- Repositorynaam/issues op GitHub ------------------------------------------------
-  issueBase: "https://github.com/geonovum/WaU/issues",
+  issueBase: "https://github.com/geonovum/WaU-UC1/issues",
   //-- edDraftURI: de URI van de werkversie van het document
-  edDraftURI: "https://github/geonovum/WaU",
+  edDraftURI: "https://geonovum.github.io/WaU-UC1",
   //-- de namen van de Editor(s) ------------------------------------------------------
   //-- vul in: per Editor: name:, company:, companyURL: -------------------------------
   
@@ -162,7 +162,7 @@ var respecConfig =
 
   //-- Optionele parameters:
   //emailComments: "mim@geonovum.nl",         // reactie mailadres, alleen bij CV!
-  //subtitle: "iets",                         // Subtitel van het document
+  subtitle: "Semantische samenhang tussen basisregistraties",                         // Subtitel van het document
   maxTocLevel: 4,                           // Aantal niveau's ToC, default is 0
   //-- LOGO: Hier kan je een ander logo opgeven indien nodig
   //logos: [{
